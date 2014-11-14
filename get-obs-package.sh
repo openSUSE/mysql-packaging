1aa9ca604b60230f91db0cda398500f466845cb9
Tomáš Chvátal <tomas.chvatal@gmail.com>
2014-11-14 14:15:17 +0100

    Add script to checkout all wored stuff from OBS

---
+++ get-obs-package.sh
@@ -0,0 +1,104 @@
+#!/usr/bin/env bash
+
+# Fetch OBS packages for all currently supported platforms
+
+help() {
+    echo "Automatic downloader for all supported mariadb/mysql packages from buildservice."
+    echo
+    echo "Using this expects you to have properly configured osc command."
+    exit 0
+}
+
+WORKDIR="$(pwd)/obsclone"
+
+SUPPORTED_UPDATES=(
+    "openSUSE:12.3:Update"
+    "openSUSE:13.1:Update"
+    "openSUSE:13.2:Update"
+)
+SUPPORTED_MARIA=(
+    "SUSE:SLE-12:GA"
+)
+SUPPORTED_MYSQL=(
+    "SUSE:SLE-11:SP3"
+)
+DEVELPROJECT="server:database"
+DEVELPKGS=(
+    "mariadb"
+    "mariadb-100"
+    "mariadb-55"
+    "mysql-community-server"
+    "mysql-community-server-55"
+    "mysql-community-server-56"
+    "mysql-community-server-57"
+)
+
+# Create new branch of the package in your home and copy it on the disk
+# param1: source prj
+# param2: package name
+checkout_package() {
+    local _branchstate=$(mktemp)
+    local _prjname=""
+
+    osc -A https://api.opensuse.org branch $1/$2 &> $_branchstate
+
+    # if the branch already exist, recurse and quit
+    if grep -q '^branch target package already exists:' $_branchstate ; then
+        _prjname=`cat $_branchstate | grep "branch target package already exists" | sed 's/^branch target package already exists: //'`
+        osc -A https://api.opensuse.org rdelete -f $_prjname -m "Replacing with new checkout" &> /dev/null
+        checkout_package $1 $2
+        return
+    elif grep -q 'A working copy of the branched package can be checked out with' $_branchstate ; then
+        _prjname="`cat $_branchstate | tail -n1 | sed 's/osc co //'`"
+    else
+	cat $_branchstate
+        echo "ERROR: branching of package $1/$2 failed"
+        exit 1
+    fi
+    pushd "${WORKDIR}" > /dev/null
+    osc -A https://api.opensuse.org co $_prjname &> /dev/null || {
+        echo "ERROR: checkout of project $_prjname failed";
+        exit 1;
+    }
+    popd > /dev/null
+    echo "Checked out package \"${WORKDIR}/$_prjname\""
+}
+
+# Prepare list of all packages we need to checkout
+checkout_packages() {
+    # common branches
+    for i in ${SUPPORTED_UPDATES[@]}; do
+        for j in "mariadb" "mysql-community-server"; do
+            checkout_package $i $j
+        done
+    done
+    # maria only
+    for i in ${SUPPORTED_MARIA[@]}; do
+        checkout_package $i mariadb
+    done
+    # mysql only
+    for i in ${SUPPORTED_MYSQL[@]}; do
+        checkout_package $i mysql
+    done
+    # develprj
+    for i in ${DEVELPKGS[@]}; do
+        checkout_package ${DEVELPROJECT} $i
+    done
+}
+
+if [[ $1 == "--help" || $1 == "-h" ]]; then
+    help
+fi
+
+# first some sanity checks
+if [[ -e "${WORKDIR}" ]]; then
+    echo "Please ensure the workdir is cleaned up for new run. Simple removing should do:"
+    echo "  rm -rf \"${WORKDIR}\""
+    exit 1
+else
+    mkdir -p "${WORKDIR}"
+fi
+
+checkout_packages
+
+
