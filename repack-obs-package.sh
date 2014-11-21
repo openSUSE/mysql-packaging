2771af142df8eff7976e430cbee25cc5e8fe44f7
Kristýna Streitová <kstreitova@suse.cz>
2014-11-21 17:55:55 +0100

    Add a option "--nodownload" for repack-obs-package.sh

--- repack-obs-package.sh
+++ repack-obs-package.sh
@@ -6,7 +6,8 @@ help() {
     echo "Run recursively the update-package script on the obs clone"
     echo
     echo "Using this expects you to have properly configured osc command."
-    echo
+    echo "Parameters:"
+    echo "	--nodownload	do not download the new source tarball"
     exit 0
 }
 
@@ -27,12 +28,17 @@ replace_source_tarball(){
 
 # Run the update script in each of the pkg dirs
 # param1: package name
+# param2: script option
 update_package() {
     echo "Working on package \"${1}\""
     pushd "${WORKDIR}/"*"/${1}" > /dev/null
     bash ../../../update-package.sh ${1} || exit 1
 
-    replace_source_tarball
+    if [[ $2 == "--nodownload" ]]; then
+        echo "Leaving the original tarball untouched"
+    else
+        replace_source_tarball
+    fi
 
     popd > /dev/null
     echo "Updated package \"${1}\""
@@ -40,9 +46,10 @@ update_package() {
 }
 
 # Run update for all of the DEVELPKGS
+# param1: script option
 update_packages() {
     for i in ${DEVELPKGS[@]}; do
-        update_package $i
+        update_package $i $1
     done
 }
 
@@ -50,4 +57,4 @@ if [[ $1 == "--help" || $1 == "-h" ]]; then
     help
 fi
 
-update_packages
+update_packages $1
